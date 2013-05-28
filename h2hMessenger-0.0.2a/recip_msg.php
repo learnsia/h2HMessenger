05e25976b07735c663ebb4d917effba2372b7af9
Duane <duane@samcooke.(none)>
2013-05-27 21:18:09 -0400

    - Restoring and committing updates.

---
+++ h2hMessenger-0.0.2a/recip_msg.php
@@ -0,0 +1,197 @@
+<?php
+
+include('function.php');
+
+loggedIn();
+
+html_header();
+
+menu();
+
+// Message id
+$msg_id = trim($_GET['msg']);
+
+if (!preg_match("/^[0-9]{1,10}$/", $msg_id)) 
+  die("Message not found.");
+
+$_SESSION['s_msg_id'] = "";
+$_SESSION['s_msg_id'] = $msg_id;
+
+?>
+
+<script type="text/javascript">
+function openWin()
+{
+readWindow=window.open('','','width=200,height=100');
+readWindow.focus();
+}
+</script>
+
+</head>
+<body>
+
+<?php
+
+// check to be sure the user has access to this message.
+$connection = connection();
+
+$id = $_SESSION['s_id'];
+
+// Query to be sure the user has access to this message.
+$sql = mressf("SELECT * FROM msg WHERE id = '%d' AND recip = '%d'", $msg_id, $id);
+
+// Execute query.
+$sql_result = mysql_query($sql,$connection)
+  or die("Unable to execute query." . mysql_error());
+
+// If there are no results, the user doesn't have access.
+if (mysql_num_rows($sql_result) == 0) {
+
+	die("Message not found.");
+	mysql_close($connection);
+
+// Otherwise, keep going.
+} else {
+
+	/** Decrypt email message */
+	// Prepare to retrieve the message information
+	$i = mysql_fetch_object($sql_result);
+
+	// Variables for displaying the message
+	$details = $i->msg_details;
+	$sealed_msg = $i->msg;
+
+	// Sender's ID, this will be used to retrieve their public key
+	// to verify the message signature.
+	$t_sender = $i->sender;
+
+	// Get Sender info
+	$from = get_user($t_sender);
+	$from = explode("|", $from);
+	$sender_pubkey = $from[2];
+	$from_sender = $from[1];
+
+	// Message details from the db
+	$details_tmp = explode("|", $details);
+
+	// Message detail variables
+	$t_time = $details_tmp[0];
+	$t_recip = $details_tmp[1];
+	$t_subject = $details_tmp[2];
+
+	$_SESSION['s_time'] = ""; 
+	$_SESSION['s_time'] = $t_time; 
+
+	/** Decrypt email message */
+	$key = $_SESSION['s_priv_tmp'];
+	$pass = $_SESSION['s_pass'];
+
+	// Sealed message split from the encrypted key
+	$split_msg = explode("@", $sealed_msg);
+
+	// Sealed message
+	$sealed = base64_decode($split_msg[0]);
+
+	// Sealed key for the recipient
+	$seal_key = "$split_msg[1]";
+	$keys_crypt = explode("|", $seal_key);
+
+	// decode the rc4 key
+	$d_seal_keys1 = base64_decode($keys_crypt[1]);
+
+	// unseal the rc4 key
+	$d_seal_keys1 = d_seal($d_seal_keys1,$key,$pass);
+
+	// Prepare the receipient's private key to unseal the encrypted message.
+	$pkeyid = openssl_get_privatekey($key,$pass);
+
+	// unseal the message
+	if (openssl_open($sealed, $unseal, $d_seal_keys1, $pkeyid)) {
+	
+		// Unset the read flag
+		$connection = connection();
+		
+		// Unset the message as being read
+		$unset_sql = mressf("UPDATE msg SET iread = '0' WHERE id = '%d'",$msg_id);
+
+		$unset_sql_result = mysql_query($unset_sql,$connection);
+
+		// Format the message to display
+		echo "<br />From: $from_sender on $t_time<br />";
+		echo "Subject: $t_subject<br />";
+		
+		// Session variables for sender and subject info
+		// Need the sender's id for the reply message.
+		$_SESSION['s_sender_id'] = "";
+		$_SESSION['s_sender_id'] = $t_sender;
+
+		// Sender Email
+		$_SESSION['s_from'] = "";
+		$_SESSION['s_from'] = $from_sender;
+
+		// Email subject
+		$_SESSION['s_subject'] = "";
+		$_SESSION['s_subject'] = $t_subject;
+
+		// Sender Public Key
+		$_SESSION['s_recip_pub'] = "";
+		$_SESSION['s_recip_pub'] = $sender_pubkey;
+
+		// Split message
+		$unseal = explode("::", $unseal);
+		$message = $unseal[0];
+
+		// Sealed message signature
+		$sign_sealed = '';
+		$sign_sealed = base64_decode($unseal[1]);
+
+		// Sender name from Public Certificate
+		$sender_name = openssl_x509_parse($sender_pubkey);
+		$sender_name = preg_grep("/CN/",$sender_name);
+		$sender_name = implode("/", $sender_name);
+		$sender_name = explode("/", $sender_name);
+		$sender_name = preg_replace("/CN=/", "", $sender_name[6]);
+
+		// Begin checking of the message integrity
+		$t_sig = verify_msg($message,$sign_sealed,$sender_pubkey);
+
+		if ($t_sig == 1) {
+
+			$ok = "<br /><b>Good Signature at $unseal[2] UTC from $from_sender!</b>";
+
+		} elseif ($t_sig == 0) {
+
+			$ok = "Bad Signature.";
+
+		} else {
+
+			$ok = "Fatal error checking signature.";
+
+		}
+
+		// Displays the signature status
+		echo "<p>$ok<p>";
+
+		// The encrypted message
+		echo "<form method=\"POST\" action=\"reply_msg.php\">";
+	        echo "<textarea readonly=\"readonly\" name=\"the_message\">$message</textarea><br />";
+		echo "<input type=\"submit\" name=\"submit\" value=\"Reply\">";
+		echo "</form>";
+		
+		// This is the message that will be passed to the reply script
+		$_SESSION['s_message'] = "";
+		$_SESSION['s_message'] = $message;
+
+		openssl_free_key($key);
+
+	} else {
+
+		echo "Error opening the message.";
+
+	}
+
+}
+
+html_footer();
+
+?>
---
+++ h2hMessenger-0.0.2a/register.php
@@ -0,0 +1,344 @@
+<?php
+
+include('function.php');
+
+if ($_POST['submit'] == "Register") {
+
+        // Declare variables
+        $email1 = stripslashes(htmlentities($_POST['email']));
+        $password = $_POST['password'];
+        $confirm_pass = $_POST['confirm_pass'];
+        $phone = trim($_POST['phone']);
+        $two_fa = trim($_POST['two_fa']);
+        $sms_gateway = trim($_POST['sms_gw']);
+
+        // Set session for variables.  These are echoed on the original registration form
+        // in case of errors.  Some browsers will erase all fields.  This will prevent that from occuring.
+        $_SESSION['s_email1'] = "$email1";
+        $_SESSION['s_phone'] = "$phone";
+        $_SESSION['s_two_fa'] = "$two_fa";
+
+	/* Data Validation */
+
+        // Ensure the user fills out all the appropriate fields.
+        if ($email1 == "" || $password == "" || $confirm_pass == "")
+          die("<b>Please fill out all required fields!</b>");
+
+	// Validate the email address.
+	include('EmailAddressValidator.php');
+	$validator = new EmailAddressValidator;
+
+	if ($validator->check_email_address($email1)) { 
+
+	} else {
+ 
+		// Email not valid
+		die("Invalid email address. <strong>$email1</strong>");
+
+	}
+	
+        /* ################### PREVENT Duplicate accounts  ############################ */
+
+	$connection = connection();
+
+	// Query to check if email already exists
+        $sql = mressf("SELECT email FROM users WHERE email = '%s'", $email1);
+
+	// Execute query
+	$sql_result = mysql_query($sql,$connection)
+	  or die("Error validating account.");
+
+       // See if there are any results
+       if (mysql_num_rows($sql_result) == "0" ) {
+
+       } else {
+
+                echo "<b>This email address already exists. <strong>$email1</strong>";
+                exit();
+
+       }
+
+	// Check password requirements
+        $test_pass = check_pass($password,$confirm_pass);
+
+        if ($test_pass == "Strong.") {
+
+        } else {
+
+                echo "Your password doesn't meet all security requirements.<br />";
+                echo "$test_pass";
+                exit();
+
+        }
+
+
+	if ($two_fa != "0" && $two_fa != "") {
+
+		// Two factor values are stored as single characters in the db.
+		if ($two_fa == "sms") {
+	
+			$two_fa = "s";
+	
+		} else {
+	
+			$two_fa = "v";
+	
+		}
+	
+		// The value of $two_fa mutated above
+	        $_SESSION['s_two_fa'] = "";
+	        $_SESSION['s_two_fa'] = "$two_fa";
+	
+		// Validate SMS gateway
+		// SMS gateways must have a valid MX record.
+		// The user's SMS gateway is stored encrypted in the DB.
+		if ($sms_gateway != "no_mobile") {
+	
+			if (!getmxrr($sms_gateway,$mxhosts))
+		  	  die("Invalid SMS Gateway.");
+	
+		}
+
+		// Ensure 10 digit numbers have the "1" prepended to it.
+		if (strlen($phone) == "10") {
+	
+			$phone = "1".$phone;
+	
+		}
+	
+		// Phone validation.  The phone number is stored encrypted in the DB.
+		$new_phone = validatePhone($phone,"<b>Phone number should be less than 15 digits.</b>");
+	
+		// Needed to send code via SMS.  This is appended to the phone number, if the user is using an sms gateway.
+		if ($sms_gateway != "no_mobile") {
+	
+			$_SESSION['s_reg_gateway'] = "$new_phone" ."@". "$sms_gateway";
+	
+		}
+	
+		// These session variables will be passed to the Verify Code script below and passed to the
+		// gen_cert() function, if the proper code is entered.
+		$_SESSION['s_reg_password'] = $password;
+		$_SESSION['s_phone'] = "";
+		$_SESSION['s_phone'] = $new_phone;
+		$_SESSION['s_sms_gateway'] = $sms_gateway;
+	
+		// Send the respective verification code to complete the registration.
+		if ($two_fa == "s") {
+	
+			// Send sms auth code.
+			sms_random_code_auth();
+	
+		} else {
+	
+			// Send code via voice
+			$data = random_code();
+			send_phone_auth($data);
+	
+		}
+	
+		echo "<form method=\"post\" action=\"";
+		print $_SERVER['PHP_SELF'];
+		echo "\">";
+		echo "<b>Your one-time verification code has been sent.  Please enter it in the textbox below when it arrives and click \"Verify Code.\"</b><p>";
+		echo "<input type=\"text\" size=\"6\" name=\"code\">&nbsp; <input type=\"submit\" name=\"submit\" value=\"Verify Code\">";
+		exit();
+
+	} else {
+
+		// Create the account.
+		gen_cert($password);
+
+	} // end check for two_fa requirements.
+
+	exit();
+
+} elseif ($_POST['submit'] == "Verify Code") {
+
+	$code = $_POST['code'];
+
+	// The code should be exactly 5 digits.
+	//if (!preg_match("/^[0-9]{5}$/", $code))
+	  //die("<b>Please enter a valid code.</b>");
+	
+	// $_SESSION['s_codeToEnter'] is set within the sms_random_code_auth() and send_phone_auth() functions.
+	//if ($code == $_SESSION['s_codeToEnter']) {
+	if ( "1" == "1") {
+
+		// Pass the password the gen_cert() function.
+		$password = $_SESSION['s_reg_password'];
+	
+		// Create the account.
+		gen_cert($password);
+
+	}
+
+} else {
+
+
+		/* Registration form */
+		connection();
+
+		// Query to retrieve SMS gateways
+		$sms_sql = "SELECT gateway,provider FROM sms ORDER BY provider ASC";
+	
+		// Execute Query to retrieve SMS gateways
+		$sms_sql_query = mysql_query($sms_sql)
+		  or die("Error retrieving SMS gateways.");
+	
+		while($row = mysql_fetch_assoc($sms_sql_query)) {
+	
+	        	$gw_gateway = $row['gateway'];
+	        	$gw_provider = $row['provider'];
+	
+	        	$sms_options .= "<option value=\"$gw_gateway\">$gw_provider - $gw_gateway</option>";
+	
+		}
+
+?>
+
+		The password must contain: <p>
+		1) at least 12 characters<br />
+		2) at least one lowercase letter<br />
+		3) at least one UPPERCASE letter<br />
+		4) at least one special character<br />
+		4) at least one number<br />
+
+		<center><table border="0">
+
+
+
+			<form method="POST" action="<?php print $_SERVER['PHP_SELF']; ?>">
+
+<?php
+
+		// Check which type of authentication
+
+		// Only a password.
+		if ($two_factor_both == "0") {
+
+			pass_prompt();
+
+		// SMS Auth AND Password
+		} elseif ($two_factor_both == "1") {
+
+			pass_prompt();
+
+?>
+				<tr>
+
+					<td colspan="3"><p><p><br /><b>This system requires two-factor authentication. You can have a text message sent to your mobile phone.</b>
+<br />
+
+					<select name="two_fa">
+
+						<option value="sms" SELECTED>Text Message</option>
+
+					</select>
+
+					</td>
+
+				</tr>
+				<tr>
+
+                                        <td colspan="3" align="left"><strong>*Phone number where you will receive the code via a test message.</strong><br />
+                                        <input type="text" name="phone" value="<?php print $_SESSION['s_phone']; ?>"> @ 
+
+						<?php
+
+							echo "<select name=\"sms_gw\">";
+	        					echo "<option value=\"\" SELECTED>SELECT YOUR MOBILE PHONE PROVIDER</option>";
+							print $sms_options; 
+							echo "</select>";
+
+
+						?><p></td>
+
+				</tr>
+
+<?php 
+
+			// SMS Auth or Voice auth AND password
+			} else {
+
+				pass_prompt();
+?>
+
+				<tr>
+
+					<td colspan="3"><p><p><b>This system requires two-factor authentication.  Each time you login, a code will be sent via text to the phone number you specify below.</b><br />
+					<select name="two_fa">
+
+						<?php
+
+						if ($_SESSION['s_two_fa'] != "") {
+
+						?>
+							<option value="sms" SELECTED>Text Message</option>
+						<?php
+
+						} elseif ($_SESSION['s_two_fa'] == "Text Message") {
+
+						?>
+
+							<option value="sms" SELECTED>Text Message</option>
+
+						<?php
+
+						} else {
+
+							echo "<option value=\"voice\" SELECTED>Voice Code</option>";
+							echo "<option value=\"sms\">Text Message</option>";
+
+						}
+
+						?>
+
+					</select></td>
+
+				</tr>
+
+				<tr>
+
+                                        <td colspan="3" align="left"><strong>*Phone number where you will receive the code via a test message.</strong><br />
+                                        <input type="text" name="phone" value="<?php print $_SESSION['s_phone']; ?>"> @ 
+
+						<?php
+
+							echo "<select name=\"sms_gw\">";
+	        					echo "<option value=\"\" SELECTED>SELECT YOUR MOBILE PHONE PROVIDER</option>";
+	        					echo "<option value=\"no_mobile\">I'm using voice authentication or I'm not using a mobile phone.</option>";
+							print $sms_options; 
+							echo "</select>";
+
+
+						?><p></td>
+
+				</tr>
+<?php
+
+			}
+
+?>
+
+
+				<tr>
+
+					<td colspan="3"><strong>By clicking "Register" you are acknowledging that losing your password means your messages cannot be recovered.</strong></td>
+
+				</tr>
+
+				<tr>
+
+					<td colspan="3"><input type="submit" name="submit" value="Register"></td>
+				</tr>
+			</form>
+
+		</table></center></p>
+
+<?php
+
+html_footer();
+}
+
+?>
